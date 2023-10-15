74c65bd15c14d7d81eabb44d58a35f38107ea69c
Broderick Riopelle <broderio@0587447171.wireless.umich.net>
2023-10-15 19:26:06 -0400

    update build script

--- build.sh
+++ build.sh
@@ -1,4 +1,48 @@
-cd client 
-idf.py build
-cd ../host
-idf.py build
+echo $1
+if [ $1 == client ]; then
+    echo "building client ..."
+    cd client
+    idf.py build
+    cd ..
+    echo "done!"
+    exit 0
+elif [ $1 == host ]; then
+    echo "building host ..."
+    cd host
+    idf.py build
+    cd ..
+    echo "done!"
+    exit 0
+elif [ $1 == tests ]; then
+    echo "building tests ..."
+    cd tests
+    for d in */ ; do
+        cd $d
+        idf.py build
+        cd ..
+    done
+    cd ..
+    echo "done!"
+    exit 0
+elif [ $1 == all ]; then
+    echo "building all ..."
+    echo "building client ..."
+    cd client
+    idf.py build
+    echo "building host ..."
+    cd ../host
+    idf.py build
+    echo "building tests ..."
+    cd ../tests
+    for d in */ ; do
+        cd $d
+        idf.py build
+        cd ..
+    done
+    cd ..
+    echo "done!"
+    exit 0
+else
+    echo "usage: ./build.sh [client|host|tests|all]"
+    exit 1
+fi
