9a4e7adaee7e771281f95b06cce55e25532e6826
Gabriel Lounsbury <lounsbg@umich.edu>
2023-10-25 14:58:13 -0400

    adc with buttons

--- gpio/main/controller.c
+++ gpio/main/controller.c
@@ -7,19 +7,25 @@
 #include "freertos/task.h"
 #include "freertos/semphr.h"
 #include "driver/gpio.h"
+#include "esp_log.h"
 
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
 #include <assert.h>
-#include "esp_adc/adc_continuous.h"
+#include "driver/adc.h"
+#include "esp_adc_cal.h"
+
+static const char *TAG = "Controller";
 
 uint32_t last_button = 0;
 uint32_t last_time = 0;
 
 static QueueHandle_t gpio_evt_queue = NULL;
+static esp_adc_cal_characteristics_t adc1_chars;
 
-static void gpio_task_example(void* arg)
+//task to print the button causing the interrupt
+static void print_task(void* arg)
 {
     uint32_t io_num;
     for (;;) {
@@ -30,6 +36,23 @@ static void gpio_task_example(void* arg)
     }
 }
 
+//task to read the values of a joystick
+void read_joystick(void* arg)
+{
+    uint32_t vertVoltage;
+    uint32_t horizVoltage;
+
+    while (1) 
+    {
+        vertVoltage = esp_adc_cal_raw_to_voltage(adc1_get_raw(ADC1_CHANNEL_3), &adc1_chars);
+        horizVoltage = esp_adc_cal_raw_to_voltage(adc1_get_raw(ADC1_CHANNEL_4), &adc1_chars);
+        ESP_LOGI(TAG, "Horizontal Value: %ld mV", horizVoltage); 
+        ESP_LOGI(TAG, "Vertical Value: %ld mV", vertVoltage);
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
+}
+
+//ISR for a button press
 static void gpio_isr_handler(void* arg)
 {
     uint32_t gpio_num = (uint32_t) arg;
@@ -40,9 +63,17 @@ static void gpio_isr_handler(void* arg)
     xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
 }
 
+//main
 void  app_main() {
+    //configure the ADC
+    esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_DEFAULT, 0, &adc1_chars);
+
+    //check for failures
+    ESP_ERROR_CHECK(adc1_config_width(ADC_WIDTH_BIT_DEFAULT));
+    ESP_ERROR_CHECK(adc1_config_channel_atten(ADC1_CHANNEL_3, ADC_ATTEN_DB_11));
+
     gpio_config_t GPIO = {};
-     //interrupt of rising edge
+     //interrupt of rising edge (release button)
     GPIO.intr_type = GPIO_INTR_POSEDGE;
     //bit mask of the pins, use GPIO4/5 here
     GPIO.pin_bit_mask = (0b1 << 9) | (0b1 << 10);
@@ -57,8 +88,6 @@ void  app_main() {
 
     //create a queue to handle gpio event from isr
     gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
-    //start gpio task
-    xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL);
 
     //hook isr handler for specific gpio pin
     gpio_isr_handler_add(9, gpio_isr_handler, (void*) 9);
@@ -69,4 +98,9 @@ void  app_main() {
     gpio_isr_handler_remove(9);
     //hook isr handler for specific gpio pin again
     gpio_isr_handler_add(9, gpio_isr_handler, (void*) 9);
+
+    //start tasks
+    //make the print preempt the adc since it happens rarely
+    xTaskCreate(print_task, "print_task", 2048, NULL, 10, NULL);
+    xTaskCreate(read_joystick, "read_joystick", 2048, NULL, 5, NULL);
 }
