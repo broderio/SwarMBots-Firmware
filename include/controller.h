1b6e4b0cd283d1bfebfb162b753815d90d2d639a
Broderick <broderio@umich.edu>
2023-11-03 17:37:53 -0400

    autoformat

--- include/controller.h
+++ include/controller.h
@@ -36,17 +36,17 @@ static bool mode = 1;
 TaskHandle_t serialMode;
 TaskHandle_t controllerMode;
 
-uint8_t* command_serializer(float vx, float vy, float wz){
+uint8_t *command_serializer(float vx, float vy, float wz)
+{
     serial_twist2D_t msg = {
         .vx = vx,
         .vy = vy,
-        .wz = wz
-    };
-    
+        .wz = wz};
+
     // Initialize variables for packet
     size_t msg_len = sizeof(msg);
-    uint8_t* msg_serialized = (uint8_t*)(malloc(msg_len));
-    uint8_t* packet = (uint8_t*)(malloc(msg_len + ROS_PKG_LENGTH));
+    uint8_t *msg_serialized = (uint8_t *)(malloc(msg_len));
+    uint8_t *packet = (uint8_t *)(malloc(msg_len + ROS_PKG_LENGTH));
 
     // Serialize message and create packet
     twist2D_t_serialize(&msg, msg_serialized);
@@ -55,36 +55,41 @@ uint8_t* command_serializer(float vx, float vy, float wz){
     return packet;
 }
 
-//ISR for a button press
-static void buttons_isr_handler(void* arg)
+// ISR for a button press
+static void buttons_isr_handler(void *arg)
 {
-    uint32_t gpio_num = (uint32_t) arg;
+    uint32_t gpio_num = (uint32_t)arg;
     uint32_t ticks = xTaskGetTickCount();
-    if ((gpio_num == last_button) && ((ticks - last_press) < 30)) return;
+    if ((gpio_num == last_button) && ((ticks - last_press) < 30))
+        return;
     last_button = gpio_num;
     last_press = ticks;
-    //xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+    // xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
     esp_now_peer_info_t *peer = malloc(sizeof(esp_now_peer_info_t));
-    if (peer == NULL) return;
-    ESP_ERROR_CHECK( esp_now_fetch_peer(true, peer));// != ESP_OK) return;
+    if (peer == NULL)
+        return;
+    ESP_ERROR_CHECK(esp_now_fetch_peer(true, peer)); // != ESP_OK) return;
     memcpy(send_param.dest_mac, peer->peer_addr, ESP_NOW_ETH_ALEN);
     free(peer);
 }
 
-//ISR for switch (change modes)
-static void switch_isr_handler(void* arg)
+// ISR for switch (change modes)
+static void switch_isr_handler(void *arg)
 {
-    uint32_t gpio_num = (uint32_t) arg;
+    uint32_t gpio_num = (uint32_t)arg;
     uint32_t ticks = xTaskGetTickCount();
-    if ((ticks - last_switch) < 100) return;
+    if ((ticks - last_switch) < 100)
+        return;
     last_switch = ticks;
-    if(mode) {
-        gpio_isr_handler_add(B1_PIN, buttons_isr_handler, (void*) B1_PIN);
-        gpio_isr_handler_add(B2_PIN, buttons_isr_handler, (void*) B2_PIN);
+    if (mode)
+    {
+        gpio_isr_handler_add(B1_PIN, buttons_isr_handler, (void *)B1_PIN);
+        gpio_isr_handler_add(B2_PIN, buttons_isr_handler, (void *)B2_PIN);
         vTaskResume(controllerMode);
         vTaskSuspend(serialMode);
     }
-    else{
+    else
+    {
         gpio_isr_handler_remove(B1_PIN);
         gpio_isr_handler_remove(B2_PIN);
         vTaskSuspend(controllerMode);
@@ -94,11 +99,12 @@ static void switch_isr_handler(void* arg)
     xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
 }
 
-void controller_init(){
-    //configure the ADC
-    // esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_DEFAULT, 0, &adc1_chars);
+void controller_init()
+{
+    // configure the ADC
+    //  esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_DEFAULT, 0, &adc1_chars);
 
-     //-------------ADC1 Init---------------//
+    //-------------ADC1 Init---------------//
     adc_oneshot_unit_init_cfg_t init_config1 = {
         .unit_id = ADC_UNIT_1,
     };
@@ -123,36 +129,36 @@ void controller_init(){
     adc_cali_create_scheme_curve_fitting(&cali_config, &JS_X_cali);
 
     gpio_config_t GPIO = {};
-     //interrupt of rising edge (release button)
+    // interrupt of rising edge (release button)
     GPIO.intr_type = GPIO_INTR_POSEDGE;
-    //bit mask of the pins, use GPIO4/5 here
+    // bit mask of the pins, use GPIO4/5 here
     GPIO.pin_bit_mask = (0b1 << B1_PIN) | (0b1 << B2_PIN);
-    //set as input mode
+    // set as input mode
     GPIO.mode = GPIO_MODE_INPUT;
-    //enable pull-up mode
+    // enable pull-up mode
     GPIO.pull_up_en = 1;
     gpio_config(&GPIO);
-    //configure switch interrupt
+    // configure switch interrupt
     GPIO.intr_type = GPIO_INTR_ANYEDGE;
-    //bit mask of the pins, use GPIO4/5 here
+    // bit mask of the pins, use GPIO4/5 here
     GPIO.pin_bit_mask = (0b1 << SW_PIN);
-    //set as input mode
+    // set as input mode
     GPIO.mode = GPIO_MODE_INPUT;
-    //enable pull-up mode
+    // enable pull-up mode
     GPIO.pull_down_en = 1;
     gpio_config(&GPIO);
 
-    //install gpio isr service
+    // install gpio isr service
     gpio_install_isr_service(0);
 
-    //create a queue to handle gpio event from isr
+    // create a queue to handle gpio event from isr
     gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
-    //hook isr handler for specific gpio pin
-    gpio_isr_handler_add(B1_PIN, buttons_isr_handler, (void*) B1_PIN);
-    //hook isr handler for specific gpio pin
-    gpio_isr_handler_add(B2_PIN, buttons_isr_handler, (void*) B2_PIN);
-    //hook isr handler for specific gpio pin
-    gpio_isr_handler_add(SW_PIN, switch_isr_handler, (void*) SW_PIN);
+    // hook isr handler for specific gpio pin
+    gpio_isr_handler_add(B1_PIN, buttons_isr_handler, (void *)B1_PIN);
+    // hook isr handler for specific gpio pin
+    gpio_isr_handler_add(B2_PIN, buttons_isr_handler, (void *)B2_PIN);
+    // hook isr handler for specific gpio pin
+    gpio_isr_handler_add(SW_PIN, switch_isr_handler, (void *)SW_PIN);
 }
 
 #endif
--- include/host.h
+++ include/host.h
@@ -1,11 +1,11 @@
 #ifndef HOST_AP_H
 #define HOST_AP_H
 
-#define UART_PORT_NUM           0
-#define JS_Y_PIN        4 // ADC1_CHANNEL3
-#define JS_X_PIN        5 // ADC1_CHANNEL4
-#define B1_PIN          9
-#define B2_PIN          10
-#define SW_PIN          17
+#define UART_PORT_NUM 0
+#define JS_Y_PIN 4 // ADC1_CHANNEL3
+#define JS_X_PIN 5 // ADC1_CHANNEL4
+#define B1_PIN 9
+#define B2_PIN 10
+#define SW_PIN 17
 
 #endif
